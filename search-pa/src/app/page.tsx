027517f62c95e63111d18576b503593fb0e082bf
Jo Seung Hyo <seunghyo7742@naver.com>
2024-02-08 18:10:13 +0900

    ✨ Feat: /courses redirection 로직 추가
    홈 page.tsx에서 /courses로 redirect될 수 있도록 수정

--- search-pa/src/app/page.tsx
+++ search-pa/src/app/page.tsx
@@ -1,7 +1,6 @@
+import { redirect } from 'next/navigation';
+
 export default function Home() {
-  return (
-    <>
-      <div className="bg-indigo-50 w-full">test</div>
-    </>
-  );
+  redirect('/courses');
+  return <></>;
 }
